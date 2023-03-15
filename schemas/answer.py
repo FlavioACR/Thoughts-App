78a2b2e37fdfadac87d47db149dc2ab64ff772a9
FlavioACR <flavioabatcarrolar@outlook.com>
2023-03-14 21:20:45 -0600

    Schema for the object answer created

--- schemas/answer.py
+++ schemas/answer.py
@@ -13,3 +13,16 @@ class Answers(BaseModel):
     response_content        : str = Field(...,min_length=25,max_length=250)
     response_date           : date = Field(...)
     
+    class Config:
+        orm_mode = True
+        
+        schema_extra = {
+            "example": {
+                "answer_id": 1,
+                "question_to_response_id": 1,
+                "user_responder_id": 1,
+                "response_content" : "Podrías cambiarlo con cuidado",
+                "response_date": str(date.today())               
+            }
+        }
+    
